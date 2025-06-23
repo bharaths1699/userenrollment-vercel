88c61398ecbe7ec181f7a16b4f487ef7c12b14d2
bharaths1699 <55750561+bharaths1699@users.noreply.github.com>
2025-06-23 14:59:04 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/com.apple.remotemanagement", (req, res) => {
 
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://172.24.125.190:9383/mdm/client/v1/enroll?encapiKey=92b6a784&templateToken=feae7c212260d79aaf3a18ad84903beb"}]}'
+  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://172.24.125.190:9383/mdm/client/v1/enroll?encapiKey=57f0da0&templateToken=855e2675a059d8534fa7f1d257e6963b"}]}'
   const inputStream = Buffer.from(responseText);
 
   const responseHeaders = {
